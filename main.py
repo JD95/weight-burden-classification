583755336c1b92d7e1f08e3d3807e4add35a5cf5
Jeff <jeffreydwyer95@hotmail.com>
2017-11-30 13:51:25 -0800

    Train using full data set

--- main.py
+++ main.py
@@ -1,59 +1,108 @@
 import numpy as np
 
 from keras.models import Sequential, Model
-from keras.layers import Dense, Input
-from keras.layers import Convolution2D, MaxPool2D
+from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D
 from keras.utils import np_utils
 
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from os import listdir
+from os.path import isfile, join
+import re
 
-train = []
 
-with open('data/curl_left(light)_000Parsed.txt') as f:
-    lines = f.readlines()
-    for line in lines[1:]:
-        try:
-            train.append(list(map(float, line.strip().split())))
-        except ValueError:
-            continue
+def parse_data_file(filepath):
+    """ String -> [[Float]] """
+    data = []
+    with open(filepath) as f:
+        lines = f.readlines()
+        for line in lines[1:]:
+            try:
+                data.append(list(map(float, line.strip().split())))
+            except ValueError:
+                continue
+    return data
 
-# Setup main training model
-input_layer = Input(shape=(78,))
-encoded = Dense(20, activation='relu')(input_layer)
-decoded = Dense(78, activation='linear')(encoded)
-model = Model(input_layer, decoded)
 
-# Encoder
-encoder = Model(input_layer, encoded)
+class DataSet ():
+    def __init__(self):
+        self.train = []
+        self.test = []
 
-# Decoder
-encoded_input = Input(shape=(20,))
-decoder_layer = model.layers[-1]
-decoder = Model(encoded_input, decoder_layer(encoded_input))
 
-# Compile Models
-model.compile(loss='mean_absolute_error',
-              optimizer='adadelta',
-              metrics=['accuracy'])
+def add_to_data_set(filepath, category, data_set):
+    """ loads a file into the dataset """
+    data = parse_data_file(filepath)
+    data_set.train.extend(data)
+    data_set.test.extend([category] * len(data))
 
-model.fit(train, train, batch_size=256, shuffle=True,
-          validation_split=0.2, epochs=100, verbose=1)
 
-score = model.evaluate(train, train, verbose=0)
+def load_data(folder):
+    """ loads data for autoencoder """
+    files = [f for f in listdir(folder) if isfile(join(folder, f))]
+    data_set = DataSet()
 
-encoded_values = encoder.predict(train)
-outputs = decoder.predict(encoded_values)
+    for f in files:
+        filepath = folder + '/' + f
+        if 'light' in f:
+            add_to_data_set(filepath, [1.0, 0.0, 0.0], data_set)
+        elif 'normal' in f:
+            add_to_data_set(filepath, [0.0, 1.0, 0.0], data_set)
+        elif 'heavy' in f:
+            add_to_data_set(filepath, [0.0, 0.0, 1.0], data_set)
+        else:
+            pass
 
-print('result is: ' + str(score))
+    print('loaded ' + str(len(data_set.train)) + ' samples')
+    return data_set
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
 
-for frame in [(0,'r'),(1,'b'),(2,'g')]:
-    ax.scatter(outputs[frame[0]][0::3], outputs[frame[0]][1::3], outputs[frame[0]][2::3], c=frame[1])
+def main():
 
-# ax.scatter(train[0][0::3], train[0][1::3], train[0][2::3], c='r')
-# ax.scatter(outputs[0][0::3], outputs[0][1::3], outputs[0][2::3], c='b')
+    data_set = load_data('data')
 
-plt.show()
+    # Setup main training model
+    input_layer = Input(shape=(78,))
+    encoded = Dense(20, activation='relu')(input_layer)
+    decoded = Dense(78, activation='linear')(encoded)
+    model = Model(input_layer, decoded)
+
+    # Encoder
+    encoder = Model(input_layer, encoded)
+
+    # Decoder
+    encoded_input = Input(shape=(20,))
+    decoder_layer = model.layers[-1]
+    decoder = Model(encoded_input, decoder_layer(encoded_input))
+
+    # Compile Models
+    model.compile(loss='mean_absolute_error',
+                  optimizer='adadelta',
+                  metrics=['accuracy'])
+
+    model.fit(data_set.train, data_set.train, batch_size=256, shuffle=True,
+              validation_split=0.2, epochs=200, verbose=1)
+
+    score = model.evaluate(data_set.train, data_set.train, verbose=0)
+
+    encoded_values = encoder.predict(data_set.train)
+    outputs = decoder.predict(encoded_values)
+
+    print('result is: ' + str(score))
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    # for frame in [(0, 'r'), (1, 'b'), (2, 'g')]:
+    #     ax.scatter(outputs[frame[0]][0::3], outputs[frame[0]]
+    #                [1::3], outputs[frame[0]][2::3], c=frame[1])
+
+    ax.scatter(data_set.train[0][0::3], data_set.train[0]
+               [1::3], data_set.train[0][2::3], c='r')
+    ax.scatter(outputs[0][0::3], outputs[0][1::3], outputs[0][2::3], c='b')
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
